0706c07bbf223c1f03951da110b78227bb9fe889
Beatriz Galarza <beatriz.glrz@gmail.com>
2026-02-05 15:13:15 -0800

    Completed two tasks in simulation

--- assistant.js
+++ assistant.js
@@ -5,28 +5,26 @@ Repository: https://github.com/beaglrz/cs81-module6b-assistant
 
 // Constructor for the Personal Assistant
 function PersonalAssistant(name) {
-  this.name = name;
+ this.name = name;
   this.tasks = [];
-  this.mood = "Neutral";
+  this.mood = "productive";
 }
 
 // Adds a Task to the List
 PersonalAssistant.prototype.addTask = function (task) {
   this.tasks.push(task);
-  console.log(`Added Task: ${task}`);
-  console.log(`Tasks Left: ${this.tasks.length}`);
 };
 
 // Completes Task in the List
 PersonalAssistant.prototype.completeTask = function () {
   if (this.tasks.length === 0) {
-    console.log("Tasks Completed");
+    console.log("No tasks left.");
     return;
   }
 
   const finishedTask = this.tasks.shift();
-  console.log(`Completing Task: ${finishedTask}`);
-  console.log(`Tasks Left: ${this.tasks.length}`);
+  console.log(`Completing task: ${finishedTask}`);
+  console.log(`Now you have ${this.tasks.length} tasks left.`);
 };
 
 // Displays Current Mood
@@ -34,5 +32,29 @@ PersonalAssistant.prototype.reportMood = function () {
   console.log(`Mood: ${this.mood}`);
 };
 
+// Simulation of Day in my Life
+
+const myAssistant = new PersonalAssistant("Trixi");
+
+console.log(`Hi! I'm ${myAssistant.name}, your personal assistant.`);
+
+// Daily Tasks
+myAssistant.addTask("Do Laundry");
+myAssistant.addTask("Cook Lunch");
+myAssistant.addTask("Go to the Gym");
+
+// Tasks Left to Complete
+console.log(`You have ${myAssistant.tasks.length} tasks left.`);
+
+// Mood While Working Through the Tasks
+myAssistant.mood = "Productive";
+myAssistant.reportMood();
+
+//Complete Task
+myAssistant.completeTask();
+
+// End of Day Mood
+myAssistant.mood = "Accomplished";
+myAssistant.reportMood();
 
 
